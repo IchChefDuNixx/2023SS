e9674675ac4fa6e60656fed094c23b36e8a60a94
IchChefDuNixx <minecraftkuchen@gmail.com>
2023-05-22 00:00:53 +0200

    updated to use app.js and handle more msg attrib.

--- oddities/ClairStone/Extension/background.js
+++ oddities/ClairStone/Extension/background.js
@@ -7,63 +7,82 @@ let port = browser.runtime.connectNative("clairstone");
 
 // On a click on the browser action, send the app a message.
 browser.action.onClicked.addListener(() => {
-  // TODO: make the button useful
-  browser.windows.create({
-    type: "detached_panel",
-    url: "about:devtools-toolbox?type=extension"
-  });
-  console.log("Sending: ping");
-  port.postMessage({data:"ping"});  
+    // TODO: make the button useful
+    browser.windows.create({
+        type: "detached_panel",
+        url: "about:devtools-toolbox?type=extension"
+    });
+    console.log("Sending: ping");
+    port.postMessage({ data: "ping" });
 });
 
 // Listen for messages from the app.
-port.onMessage.addListener(function(message) {
+port.onMessage.addListener(function (message) {
 
-  if (message.data != "DONTLOG") {
-    // Handle the completion message here
-    console.log("Python script completed with message:", message.data);
-  }
-
-  if (message.toHash) {
-    // hash_encode({commander:{id:431,level:6,runes:[]}, deck:[{id:26133,level:6,runes:[]}]})
-    if (message.toHash) {
-      const numsims = 10000;
-      const bges = "164,165";
-      const use_tower = true;
-
-      let deck1;
-      fetch('http://localhost:3000?hashable=' + JSON.stringify(message.toHash.player), {mode: 'cors'})
-      .then(response => response.text())
-      .then(result => {
-        deck1 = result;
-        return fetch('http://localhost:3000?hashable=' + JSON.stringify(message.toHash.opponent), {mode: 'cors'})
-      })
-      .then(response => response.text())
-      .then(result => {
-        const deck2 = result;
-        return fetch(`http://localhost:3001?deck1=${deck1}&deck2=${deck2}&use_tower=${use_tower}&bges=${bges}&numsims=${numsims}`);
-      })
-      .then(response => response.text())
-      .then(result => {
-        console.log(result);
-      })
-      .catch(error => {
-        console.error("Error:", error);
-      })
+    if (message.data != "DONTLOG") {
+        // Handle the completion message here
+        console.log("Python script completed with message:", message.data);
     }
-  }
 
-  // show(message);
+    if (message.toHash) {
+        // hash_encode({commander:{id:431,level:6,runes:[]}, deck:[{id:26133,level:6,runes:[]}]})
+        if (message.toHash) {
+            const numsims = 30000;
+            const use_tower = message.is_tower_battle === true; // true iff tower battle, else false
+            var bges = message.bges;
+            var hand = []
+            if (message.hand) {
+                hand = message.hand;
+            } else {
+                hand = [5,6,7];
+            }
+            playerIsHost = message.playerIsHost
+
+            let deck1;
+            fetch('http://localhost:3000/hash?hashable=' + JSON.stringify(message.toHash.player), { mode: 'cors' })
+                .then(response => response.text())
+                .then(result => {
+                    deck1 = result;
+                    return fetch('http://localhost:3000/hash?hashable=' + JSON.stringify(message.toHash.opponent), { mode: 'cors' })
+                })
+                .then(response => response.text())
+                .then(result => {
+                    let deck2 = result;
+                    // TODO: return this value from readcards.py
+                    // also handle sim.js to track the opponent's first pick, not the player's
+                    // if (!message.playerIsHost) {
+                    //     const tempDeck = deck1;
+                    //     deck1 = deck2;
+                    //     deck2 = tempDeck;
+                    // }
+                    // // TODO: return this value from readcards.py
+                    // if (message.requestType && message.requestType == "arena") {
+                    //     bges += "521";
+                    // }
+                    return fetch(`http://localhost:3000/sim?deck1=${deck1}&deck2=${deck2}&use_tower=${use_tower}&bges=${bges}&numsims=${numsims}&hand=${hand}`);
+
+                })
+                .then(response => response.text())
+                .then(result => {
+                    console.log(result);
+                })
+                .catch(error => {
+                    console.error("Error:", error);
+                })
+        }
+    }
+
+    // show(message);
 
 });
 
 // Handle errors
-port.onDisconnect.addListener(function() {
-  if (browser.runtime.lastError) {
-    console.error("Disconnected due to an error:", browser.runtime.lastError);
-  } else {
-    console.log("Port disconnected.");
-  }
+port.onDisconnect.addListener(function () {
+    if (browser.runtime.lastError) {
+        console.error("Disconnected due to an error:", browser.runtime.lastError);
+    } else {
+        console.log("Port disconnected.");
+    }
 });
 
 
@@ -71,38 +90,38 @@ port.onDisconnect.addListener(function() {
 
 function listener(details) {
 
-  let filter = browser.webRequest.filterResponseData(details.requestId);
-  let decoder = new TextDecoder("utf-8");
-  let encoder = new TextEncoder();
-  let responseDataChunks = [];
+    let filter = browser.webRequest.filterResponseData(details.requestId);
+    let decoder = new TextDecoder("utf-8");
+    let encoder = new TextEncoder();
+    let responseDataChunks = [];
 
-  // intercept any response that matches the urls
-  filter.ondata = (event) => {
-    let str = decoder.decode(event.data, {stream: false});
-    responseDataChunks.push(str);
-    // console.log('ondata event: ', str.length); // Add a log statement to check the ondata event
-  }
+    // intercept any response that matches the urls
+    filter.ondata = (event) => {
+        let str = decoder.decode(event.data, { stream: false });
+        responseDataChunks.push(str);
+        // console.log('ondata event: ', str.length); // Add a log statement to check the ondata event
+    }
 
-  // and pass them to the server
-  filter.onstop = (event) => {
-    let responseData = responseDataChunks.join("");
-    // console.log('onstop event: ', responseData.length, "(", responseDataChunks.length, "chunk(s) )"); // Add a log statement to check the onstop event
-    port.postMessage({"url": details.url, "data": responseData})
-    filter.write(encoder.encode(responseData));
-    filter.disconnect();
-  }
+    // and pass them to the server
+    filter.onstop = (event) => {
+        let responseData = responseDataChunks.join("");
+        // console.log('onstop event: ', responseData.length, "(", responseDataChunks.length, "chunk(s) )"); // Add a log statement to check the onstop event
+        port.postMessage({ "url": details.url, "data": responseData })
+        filter.write(encoder.encode(responseData));
+        filter.disconnect();
+    }
 
-  return {};
+    return {};
 }
 
 browser.webRequest.onBeforeRequest.addListener(
-  listener,
-  {urls: ["*://*.synapse-games.com/*"]},
-  ["blocking"]
+    listener,
+    { urls: ["*://*.synapse-games.com/*"] },
+    ["blocking"]
 )
 
 function show(message) {
-  browser.tabs.query({active: true, currentWindow: true}).then((tabs) => {
-    browser.tabs.sendMessage(tabs[0].id, {type: "console", content: message});
-  });
+    browser.tabs.query({ active: true, currentWindow: true }).then((tabs) => {
+        browser.tabs.sendMessage(tabs[0].id, { type: "console", content: message });
+    });
 }
